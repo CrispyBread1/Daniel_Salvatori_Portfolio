4d208239a314cba18d26191973bf8a24f69080df
Daniel <dsalvatori2008@gmail.com>
2023-02-16 16:32:04 +0000

    finsihed adding me to contact me

--- src/containers/cv.js
+++ src/containers/cv.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from "react"
 import './cv.css'
 import FrogBack from '../imgs/frog_back.png'
+import Me from '../imgs/IMG_3439.JPG'
 
 
 const CV = ({backToHome}) => {
@@ -26,7 +27,9 @@ const CV = ({backToHome}) => {
                     'For a short time when I was fifteen I lived in China for the summer, and later spent a term abroad in Toronto, allowing me to confidently connect and communicate with people. I like to build on interpersonal experiences through social and constructive collaboration with others. In my free time I tend to be working on photography projects, designing/rebuilding old coding applications, reading or baking. When I was around ten my Grandad introduced to me my first programming language HTML. Using this I made a very basic fact website about  my favourite animal, penguins. Later in life I became fascinated by the idea of stocks, while everyone was sending ‘Blockbuster’ stock to the moon. This led me to become intrigued by the idea of implementing code to track, predict, buy and sell stocks. Thus leading me down the path I am now set on conquering, a career in software development.'
                 </p>
             </div>
-
+            <div className="PhotoMe">
+                <img className="Photo" src={Me} width="130em" height="auto"/>
+            </div>
             <div className="ContactDetails">
                 <p className="Email">Email: dsalvatori2008@gmail.com</p>
                 <ul className="ContactList">
