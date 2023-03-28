2b1346fc8875e63729d90891c105d23de3bc29c9
Daniel <dsalvatori2008@gmail.com>
2023-03-28 16:10:53 +0100

    changed portfolio

--- src/containers/projectBoard.js
+++ src/containers/projectBoard.js
@@ -2,17 +2,18 @@ import React from "react"
 import './projectBoard.css'
 import FrogBack from '../imgs/frog_back.png'
 import Project from "../components/project"
-import breakApp from '../imgs/projects/Break_time_app.png'
+// import breakApp from '../imgs/projects/Break_time_app.png'
+import robit from '../imgs/projects/Robit.png'
 import animalAdoption from '../imgs/projects/animal_adoption.png'
 import stocksApp from '../imgs/projects/stocks_app.png'
 
 const ProjectBoard = ({backToHome}) => {
     
-    const breakTimeAppGit = 'https://github.com/CrispyBread1/Breaktime_App.git'
+    const breakTimeAppGit = 'https://github.com/CrispyBread1/Robit.git'
     const animalAdopGit = 'https://github.com/CrispyBread1/Animal-adoption.git'
     const stocksGit = 'https://github.com/CrispyBread1/Stock_Analysis_js.git'
 
-    const titles = ['BreakTime Tracker', 'Animal Adoption App', 'Stock Probability Calculator']
+    const titles = ['Robit', 'Animal Adoption App', 'Stock Probability Calculator']
 
     const moveToHome = () => {
         const id = document.querySelector('[id="Project"]')
@@ -32,7 +33,7 @@ const ProjectBoard = ({backToHome}) => {
             </div>
 
             <ul className="Projects">
-                <li className="SingleProject"><Project projetIMG={breakApp} gitHubLink={breakTimeAppGit} title={titles[0]}/></li>
+                <li className="SingleProject"><Project projetIMG={robit} gitHubLink={breakTimeAppGit} title={titles[0]}/></li>
                 <li className="SingleProject"><Project projetIMG={animalAdoption} gitHubLink={animalAdopGit} title={titles[1]}/></li>
                 <li className="SingleProject"><Project projetIMG={stocksApp}gitHubLink={stocksGit} title={titles[2]}/></li>
             </ul>
