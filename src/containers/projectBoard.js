fb5eebd850c67a52ebb7623d8d2ac8c6fcc9af52
CrispyBread1 <dsalvatori2008@gmail.com>
2025-07-15 10:58:59 +0100

    Updated photos and links

--- src/containers/projectBoard.js
+++ src/containers/projectBoard.js
@@ -2,18 +2,19 @@ import React from "react"
 import './projectBoard.css'
 import FrogBack from '../imgs/frog_back.png'
 import Project from "../components/project"
-// import breakApp from '../imgs/projects/Break_time_app.png'
 import robit from '../imgs/projects/Robit.png'
-import BrookFarm from '../imgs/projects/BrookFarm.png'
-import stocksApp from '../imgs/projects/stocks_app.png'
+import salvatori_admin from '../imgs/projects/salvatori_admin/admin_1.png'
+import salvatori_stock from '../imgs/projects/salvatori_stock/stock_1.png'
+
+
 
 const ProjectBoard = ({backToHome}) => {
     
-    const breakTimeAppGit = 'https://github.com/CrispyBread1/Robit.git'
-    const brookFarmGlamping = 'https://github.com/CrispyBread1/brooke_farm_glamping'
-    const stocksGit = 'https://github.com/CrispyBread1/Stock_Analysis_js.git'
+    const salvatoriButchersAdminGit = 'https://github.com/CrispyBread1/Salvatori_Butchers'
+    const salvatoriStockAppGit = 'https://github.com/CrispyBread1/Salvatori_butchers_stock'
+    const robitAppGit = 'https://github.com/CrispyBread1/Robit.git'
 
-    const titles = ['Robit', 'Brook Farm Glamping', 'Stock Probability Calculator']
+    const titles = ['Salvatori Food Service: App', 'Salvatori Food Service: Desktop', 'Robit' ]
 
     const moveToHome = () => {
         const id = document.querySelector('[id="Project"]')
@@ -25,18 +26,16 @@ const ProjectBoard = ({backToHome}) => {
     return (
         
         <div id="Project" value="Project">
-            {/* <h1>Projet board</h1> */}
             <button className="ProjectToReturn" onClick={moveToHome} >Back</button>
-            {/* <h5 className="ProjectToReturn" onClick={moveToHome}>click here to return</h5> */}
 
             <div className= "Frog-back-image-projects">
                     <img className="Frog-Back-Image-Projectboard"src={FrogBack} />
             </div>
 
             <ul className="Projects">
-                <li className="SingleProject"><Project projetIMG={robit} gitHubLink={breakTimeAppGit} title={titles[0]}/></li>
-                <li className="SingleProject"><Project projetIMG={BrookFarm} gitHubLink={brookFarmGlamping} title={titles[1]}/></li>
-                <li className="SingleProject"><Project projetIMG={stocksApp}gitHubLink={stocksGit} title={titles[2]}/></li>
+                <li className="SingleProject"><Project projetIMG={salvatori_admin} gitHubLink={salvatoriButchersAdminGit} title={titles[0]}/></li>
+                <li className="SingleProject"><Project projetIMG={salvatori_stock} gitHubLink={salvatoriStockAppGit} title={titles[1]}/></li>
+                <li className="SingleProject"><Project projetIMG={robit} gitHubLink={robitAppGit} title={titles[2]}/></li>
             </ul>
             
         </div>
